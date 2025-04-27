4d7e8ec6ebe3cfc9a3c481adfd8ec27152b904fa
Jayadeveloper0612 <jayakurre@iitbhilai.ac.in>
2025-04-27 11:10:13 +0530

    React + Vite Project Setup with tailwind css for UI/UX

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./index.css"
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen border-2 border-red-500 font-bold text-2xl text-emerald-900 underline">
+        Hello World!
+      </div>
+    </>
+  )
+}
+
+export default App
